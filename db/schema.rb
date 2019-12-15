58b29bca79f286a504f6bc1f1791df5f10dedb0f
Juan Carlo Banayo <johncban@gmail.com>
2019-12-15 05:58:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,16 @@ ActiveRecord::Schema.define(version: 2019_12_15_041058) do
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.text "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
