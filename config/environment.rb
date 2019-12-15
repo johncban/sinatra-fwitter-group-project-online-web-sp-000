d53374a7c401d807c27f7ba3adb338c0b8844e1d
Juan Carlo Banayo <johncban@gmail.com>
2019-12-15 07:17:49 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-require 'require_relative'
+
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
